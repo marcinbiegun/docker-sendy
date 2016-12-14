3498f99d50d29066edabc1da1536128e418fc6fb
Marcin Biegun <marcin@marcinbiegun.com>
2016-12-14 21:54:50 +0100

    Code grab from mortezaPRK/docker-php

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # docker-sendy
-Docker deployment Sendy newsletter service 
+
+Docker deployment for Sendy newsletter app.
+
+## Thanks
+
+Based on [mortezaPRK/docker-php](https://github.com/mortezaPRK/docker-php)
---
+++ public_html/index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Uncomment following lines to Test error configuration (i.e. error_reporting, error_log, log_errors, display_errors and etc..)
+ */
+var_export($i);
+
+/**
+ * Testing Confiuration
+ */
+phpinfo();
